de36793b7a02916bdd64a2f456574d2bfbbe9d7c
Iryna Rudzko <irynarudzko@MacBook-Air-Iryna.local>
2021-03-15 13:12:31 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  for (let i = str.length; i >= 0; i--) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+
+      while (str.includes(bracketsConfig[j].join(''))) {
+        str = str.replace(bracketsConfig[j].join(''), '');
+      }
+    }
+  }
+  return str === '' ? true : false;
+};
